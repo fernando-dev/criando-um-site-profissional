7e0e87524269223859da6d5bf8262f410de75b1f
Fernando Moreira <fernando.moreira.costa@gmail.com>
2012-12-15 14:31:05 -0200

    Atualizações dos arquivos - Aulas 4 e 5

---
+++ README.txt
@@ -0,0 +1,35 @@
+=== "Criando um site profissional e completo" ===
+
+Tópicos:
+1 - Fatiando imagens necessárias no photoshop
+        1.2 - Download WinInfo Layout PSD
+                fernandoprogrammer.wordpress.com
+
+2 - Grid LinhaFramework (linhaframework.org)
+        2.1 - reset.css
+        2.2 - grid.css
+
+3 - Usando HTML5
+
+4 - Montando o site em tableless
+        4.1 - Home
+        4.2 - Sobre
+        4.3 - Serviços
+        4.4 - Portfolio
+        4.5 - Contato
+
+5 - Usando plug-ins jQuery
+        5.1 - html5shiv
+        5.2 - Nivo Slider
+        5.3 - Slidetabs (LinhaFramework )
+        5.4 - Tooltip (LinhaFramework )
+        5.5 - Valida (LinhaFramework )
+        5.6 - Fancybox
+
+6 - Fatiando e estruturando no site em PHP
+
+7 - URL amigáveis com .htaccess e PHP
+
+8(final) - Enviando o site para o servidor e testando o formulário de contato
+
+http://fernandomoreiraweb.com/exemplos/va-criando-um-site-completo
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8" />
+    <title> Video Aula: Desenvolvendo um site Profissional e Completo </title>
+    
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="index, follow" />
+    <meta name="revisit-after" content="1 day" />
+    <meta name="author" content="" />
+    <meta name="contact" content="" />
+    
+    <link type="text/css" rel="stylesheet" href="public/css/style.css" />   
+    <link type="text/css" rel="stylesheet" href="public/css/nivo-slider.css" /> 
+    
+</head>
+<body>
+    <div id="main" class="grid-12">
+        <header id="header" class="tamanho12">
+            <a href="#" class="logo left tamanho4" title="Video Aula: Desenvolvendo um site Profissional e Completo"> 
+                Video Aula: Desenvolvendo um site Profissional e Completo
+            </a>
+            <div class="right tamanho4">
+            
+                <div id="social">
+                    <a href="http://linkedin.com/" class="linkedin" target="_blank" title="Linkedin">linkedin</a>
+                    <a href="http://facebook.com/" class="facebook" target="_blank" title="Facebook">facebook</a>
+                    <a href="http://twitter.com/" class="twitter" target="_blank" title="Twitter">twitter</a>
+                </div>
+                
+                <div class="clear"></div>
+                
+                <div id="search">
+                    <form method="get" name="search" id="formsearch" action="">
+                        <input type="text" name="s" id="inputsearch" placeholder="Procurar..." />
+                    </form>
+                </div>
+                
+            </div>
+        </header>
+        
+        <nav id="menu">
+        	<ul>
+        		<li><a href="#">Home</a></li>
+        		<li><a href="#">Sobre Nós</a></li>
+        		<li><a href="#">Serviços</a></li>
+        		<li><a href="#">Produtos</a></li>
+        		<li><a href="#">Contato</a></li>
+        	</ul>
+        </nav>
+        
+        <section class="slider-wrapper tamanho12">
+        	<div id="slider">
+        		<img src="public/uploads/banner/banner-1.jpg" alt="banner1" />
+        		<img src="public/uploads/banner/banner-2.jpg" alt="banner2" />
+        		<img src="public/uploads/banner/banner-3.jpg" alt="banner3" />
+        	</div>
+        </section>
+        
+    </div>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script type="text/javascript" src="public/js/lib/nivo-slider.js"></script>
+    <script src="public/js/scripts.js"></script>
+</body>
+</html>
